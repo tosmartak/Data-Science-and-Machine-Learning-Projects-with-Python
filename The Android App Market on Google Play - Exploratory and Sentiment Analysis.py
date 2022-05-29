9b5f4995fc51bac159c6961694cb5c9a13c73a1f
Tosin Harold Akingbemisilu <69729533+tosmartak@users.noreply.github.com>
2022-05-29 16:27:03 +0100

    Update The Android App Market on Google Play - Exploratory and Sentiment Analysis.py

--- The Android App Market on Google Play - Exploratory and Sentiment Analysis.py
+++ The Android App Market on Google Play - Exploratory and Sentiment Analysis.py
@@ -212,9 +212,7 @@ plotly.offline.iplot({'data': data, 'layout': layout})
 
 # ## 10. Sentiment analysis of user reviews
 # Mining user review data to determine how people feel about your product, brand, or service can be done using a technique called sentiment analysis. User reviews for apps can be analyzed to identify if the mood is positive, negative or neutral about that app. For example, positive words in an app review might include words such as 'amazing', 'friendly', 'good', 'great', and 'love'. Negative words might be words like 'malware', 'hate', 'problem', 'refund', and 'incompetent'.
-
 # By plotting sentiment polarity scores of user reviews for paid and free apps, we observe that free apps receive a lot of harsh comments, as indicated by the outliers on the negative y-axis. Reviews for paid apps appear never to be extremely negative. This may indicate something about app quality, i.e., paid apps being of higher quality than free apps on average. The median polarity score for paid apps is a little higher than free apps, thereby syncing with our previous observation.
-
 # In this notebook, we analyzed over ten thousand apps from the Google Play Store. We can use our findings to inform our decisions should we ever wish to create an app ourselves.
 
 # Load user_reviews.csv
